5aee7533597d4d3894b72c2738635f9fc967809b
harshpatanwadia <56440819+harshpatanwadia@users.noreply.github.com>
2023-01-30 10:44:44 +0530

    Update prog.java

--- prog.java
+++ prog.java
@@ -1,5 +1,5 @@
     public class LengthExample{  
-    public static void main(String args[]){  
+    public stsatic void main(String args[]){  
     String s1="javatpoint";  
     String s2="python";  
     System.out.println("string length is: "+s1.length());//10 is the length of javatpoint string  
